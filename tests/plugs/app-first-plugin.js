40319abb9ec42ac297ddec026fe322849c3e54b4
msdoom <msdoom2011@gmail.com>
2015-04-15 13:37:21 +0300

    Fixed bug

--- tests/plugs/app-first-plugin.js
+++ tests/plugs/app-first-plugin.js
@@ -2,11 +2,11 @@ var appFirstPlugin = Subclass.createModule('appFirstPlugin', {
     plugin: true,
     parameters: {
         mode_bad_name: "prod"
-    },
-    onConfig: function() {
-        console.log('fsjdklfjslkdf');
-        //var parameterManager = this.getParameterManager();
-        //parameterManager.renameParameter('mode_bad_name', 'mode');
-        //console.log(Object.keys(parameterManager.getParameters()));
     }
 });
+
+appFirstPlugin.onConfig(function() {
+    var parameterManager = this.getParameterManager();
+    parameterManager.renameParameter('mode_bad_name', 'mode');
+});
+
